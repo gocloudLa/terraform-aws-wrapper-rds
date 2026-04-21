682be06d7200101dd95d2f1b2a001062ae387a3b
Ignacio Bisio <ibisio@gocloud.la>
2026-04-21 13:05:06 -0300

    fix(example): change period and evaluations in CPUCreditBalance alarm (#23)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -297,14 +297,14 @@ module "wrapper_rds" {
         # }
         # "warning-CPUCreditBalance" = {
         #   description = "RDS CPUCreditBalance below 12 creditcs"
-        #   # This alarm helps to monitor the number of earned CPU credits that an instance has accrued since it was launched or started. 
+        #   # This alarm helps to monitor the number of earned CPU credits that an instance has accrued since it was launched or started. The min period you can use in this metric is 300 seconds (5 min).
         #   threshold   = 12
         #   unit        = "Count"
         #   metric_name = "CPUCreditBalance"
         #   statistic   = "Average"
         #   namespace   = "AWS/RDS"
-        #   period      = 60
-        #   evaluation_periods = 3
+        #   period      = 300
+        #   evaluation_periods = 4
         #   datapoints_to_alarm = 3
         #   comparison_operator = "LessThanThreshold"
         #   alarms_tags = {
@@ -313,14 +313,14 @@ module "wrapper_rds" {
         # }
         # "critical-CPUCreditBalance" = {
         #   description = "RDS CPUCreditBalance below 30 credits"
-        #   # This alarm helps to monitor the number of earned CPU credits that an instance has accrued since it was launched or started.
+        #   # This alarm helps to monitor the number of earned CPU credits that an instance has accrued since it was launched or started. The min period you can use in this metric is 300 seconds (5 min).
         #   threshold   = 30
         #   unit        = "Count"
         #   metric_name = "CPUCreditBalance"
         #   statistic   = "Average"
         #   namespace   = "AWS/RDS"
-        #   period      = 60
-        #   evaluation_periods = 3
+        #   period      = 300
+        #   evaluation_periods = 4
         #   datapoints_to_alarm = 3
         #   comparison_operator = "LessThanThreshold"
         #   alarms_tags = {
